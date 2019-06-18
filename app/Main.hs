a831d01ba8f29851f5bae6dc04cd108c49fc2710
Sandy Maguire <sandy@sandymaguire.me>
2019-06-18 14:45:39 -0400

    Code review

--- app/Main.hs
+++ app/Main.hs
@@ -3,82 +3,61 @@
 module Main where
 
 import Polysemy
-import Polysemy.Error
 import Polysemy.Input
 import Polysemy.Output
-import Polysemy.Reader
 import Polysemy.State
-import Prelude hiding (readLn)
-import System.Random (newStdGen, randomRs)
+import Polysemy.Trace
+import System.Random (newStdGen, randomRs, randomRIO)
 import Text.Read (readMaybe)
 
-
-newtype RandState = RandState { unRandState :: [Int] }
-  deriving Show
 
 newtype GuessConfig = GuessConfig { unGuessConfig :: (Int, (Int, Int)) }
   deriving Show
 
-data Guess m a where
-  ReadLn :: Guess m String
-  WriteLn :: String -> Guess m ()
-  GetConfig :: Guess m GuessConfig
-  GetNextRand :: Guess m Int
-
-makeSem ''Guess
-
-
-runGuessIO :: Members '[Reader GuessConfig, State RandState, Lift IO] r => Sem (Guess ': r) a -> Sem r a
-runGuessIO = interpret \case
-  ReadLn -> sendM getLine
-  WriteLn s -> sendM . putStrLn $ '>':' ':s
-  GetConfig -> ask
-  GetNextRand -> do
-    (h,rest) <- gets $ \(RandState (h:rest)) -> (h,rest)
-    put $ RandState rest
-    return h
+runListInputForever :: [i] -> Sem (Input i ': r) a -> Sem r a
+runListInputForever is = fmap snd . runState (cycle is) . reinterpret \case
+  Input -> do
+    gotten <- get
+    let (s : ss) = gotten  -- safe because the state is infinite
+    put ss
+    pure s
 
 
-runGuessPure :: Members '[Reader GuessConfig, State RandState] r => [String] -> Sem (Guess ': r) a -> Sem r ([String], Either String a)
-runGuessPure inp = runFoldMapOutput pure . runListInput inp . runError . reinterpret3 \case
-  ReadLn -> input >>= maybe (throw "Failed to read") return
-  WriteLn s -> output $ '>':' ':s
-  GetConfig -> ask
-  GetNextRand -> do
-    (h,rest) <- gets $ \(RandState (h:rest)) -> (h,rest)
-    put $ RandState rest
-    return h
+tracePrefix :: Member Trace r => Sem r a -> Sem r a
+tracePrefix = intercept \case
+  Trace msg -> trace $ "> " ++ msg
 
 
-guessProg :: forall r. Member Guess r => Sem r [Bool]
-guessProg = go True []
+
+guessProg :: forall r. Members '[Input String, Input Int, Trace] r => GuessConfig -> Sem r [Bool]
+guessProg config = go True []
   where
     -- Note: Because of ScopedTypeVariables, r and its constraints are available in the function body.
     go :: Bool -> [Bool] -> Sem r [Bool]
     go False record = return $ reverse record
     go True record = do
-      GuessConfig (guessesallowed, (minguess, maxguess)) <- getConfig
-      writeLn ""
-      writeLn $ "I'm thinking of a number between " <> show minguess <> " and " <> show maxguess <> ". Guesses allowed: " <> show guessesallowed
-      correct <- getNextRand
+      let GuessConfig (guessesallowed, (minguess, maxguess)) = config
+      trace ""
+      trace $ "I'm thinking of a number between " <> show minguess <> " and " <> show maxguess <> ". Guesses allowed: " <> show guessesallowed
+      correct <- input
       let
         doGuess :: Int -> Sem r (Maybe Int)
         doGuess remaining = do
           let guessnum = (guessesallowed - remaining) + 1
-          writeLn $ "Enter guess #" <> show guessnum <> ":"
-          maybeguess <- readMaybe <$> readLn
+          trace $ "Enter guess #" <> show guessnum <> ":"
+          maybeguess <- readMaybe <$> input
           case maybeguess of
             Just guess
-              | guess > correct -> writeLn $ show guess <> " is too high."
-              | guess < correct -> writeLn $ show guess <> " is too low."
+              | guess > correct -> trace $ show guess <> " is too high."
+              | guess < correct -> trace $ show guess <> " is too low."
               | otherwise -> return ()
-            Nothing -> writeLn "That's not a valid number. You just wasted a guess!"
+            Nothing -> trace "That's not a valid number. You just wasted a guess!"
           return maybeguess
 
         doGuesses :: Int -> Sem r Bool
-        doGuesses (-1) = writeLn "You win!" >> return True
+        doGuesses (-1) = trace "You win!" >> return True
         doGuesses 0 = do
-          writeLn $ "You ran out of guesses. Game over! The number was " <> show correct <> "."
+          trace $ "You ran out of guesses. Game over! The number was " <> show correct <> "."
           return False
         doGuesses remaining = do
           maybeguess <- doGuess remaining
@@ -86,13 +65,13 @@ guessProg = go True []
 
         playAgain :: Sem r Bool
         playAgain = do
-          writeLn ""
-          writeLn "Play again? (Y/n)"
-          (`elem` ["y", "yes", "hokay", ""]) <$> readLn
+          trace ""
+          trace "Play again? (Y/n)"
+          (`elem` ["y", "yes", "hokay", ""]) <$> input
 
       won <- doGuesses guessesallowed
       again <- playAgain
-      writeLn $ if again then "Starting a new game!" else "Goodbye!"
+      trace $ if again then "Starting a new game!" else "Goodbye!"
       go again (won:record)
 
 
@@ -103,22 +82,29 @@ main = do
     guessesallowed = 5
     guessmin = 1
     guessmax = 100
-    conf = GuessConfig (guessesallowed, (guessmin, guessmax))
-    randlist = RandState $ randomRs (guessmin, guessmax) rgen
+    bounds = (guessmin, guessmax)
+    conf = GuessConfig (guessesallowed, bounds)
+    randlist = randomRs (guessmin, guessmax) rgen
     inputlist = ["50", "75", "", "", "", "y","75"]
-    presult :: (RandState, ([String], Either String [Bool]))
-    presult = run . runReader conf . runState randlist $ runGuessPure inputlist guessProg
+    (progoutput :: [String], progreturn)
+            = run
+            . runFoldMapOutput (:[])
+            . runTraceAsOutput
+            . tracePrefix
+            . runListInputForever randlist
+            . runListInputForever inputlist
+            $ guessProg conf
 
   putStrLn "====== Result from pure program"
-  case snd presult of
-    (progoutput, Left err) -> do
-      mapM_ print progoutput
-      putStrLn $ "Completed with error: " <> err
-    (progoutput, Right progreturn) -> do
-      mapM_ print progoutput
-      putStrLn $ "Successful completion with result: " <> show progreturn
+  mapM_ print progoutput
+  putStrLn $ "Successful completion with result: " <> show progreturn
   putStrLn "\n"
   putStrLn "====== Running IO program"
-  iresult <- fmap snd . runM . runReader conf . runState randlist $ runGuessIO guessProg
+  iresult <- runM
+           . runTraceIO
+           . tracePrefix
+           . runMonadicInput (sendM $ randomRIO bounds)
+           . runMonadicInput (sendM getLine)
+           $ guessProg conf
   putStrLn "====== Result from IO program"
   putStrLn $ "Completion with result: " <> show iresult
